f07d9af22cbcca8edd0e5aab5c08798143029061
jiheum <ajjarago@naver.com>
2026-06-20 13:41:42 +0900

    테스트

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 
 import streamlit as st
-# 테스트
+
 PROJECT_ROOT = Path(__file__).resolve().parent
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
